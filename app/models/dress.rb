fdfe73cc7841b571b53ce96b20bc737a8ec9d3cc
beekimrj <beekimrj@yahoo.com>
2020-06-01 23:02:36 +0545

    add validation for image upload, enable to add jpg and png files only

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -1,4 +1,13 @@
 class Dress < ApplicationRecord
 	has_one_attached :dress_img
+	validate :acceptable_image
+
+	def acceptable_image
+		return unless dress_img.attached?
+		acceptable_types = ["image/jpeg","image/png"]
+		unless acceptable_types.include?(dress_img.content_type)
+    errors.add(:dress_img, "must be a JPEG or PNG")
+  	end
+	end
 
 end

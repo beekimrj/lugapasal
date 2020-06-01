fd6ed8922a1a876c1b7855856cd91b9c5be90638
beekimrj <beekimrj@yahoo.com>
2020-06-01 19:19:51 +0545

    add navbar and CRUD for dress without image

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -3,15 +3,44 @@ class DressesController < ApplicationController
     @dresses = Dress.all;
   end
 
+  def new
+    @dress = Dress.new;
+  end
+
   def create
+    @dress = Dress.new(dress_params)
+    if @dress.save
+      redirect_to dress_path(@dress)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @dress = Dress.find(params[:id])
   end
 
   def show
+    @dress = Dress.find(params[:id])
   end
 
   def update
+    @dress = Dress.find(params[:id])
+    if @dress.update(dress_params)
+      redirect_to @dress
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @dress = Dress.find(params[:id])
+    @dress.destroy
+    redirect_to root_path
   end
+
+  private
+    def dress_params
+      params.require(:dress).permit(:name, :size, :color, :description,:price)
+    end
 end
